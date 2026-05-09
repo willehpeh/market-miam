1d72f600f71dffadc65880b7e1686dcd9de90b15
w.alexander <will_alexander@me.com>
2026-05-09 22:47:41 +0200

    Add Calendar object

--- packages/market-days/src/register-market-schedule/register-market-schedule.handler.ts
+++ packages/market-days/src/register-market-schedule/register-market-schedule.handler.ts
@@ -1,21 +1,40 @@
-import { EventStore } from '@market-monster/event-sourcing';
+import { Aggregate, EventStore } from '@market-monster/event-sourcing';
 import { RegisterMarketSchedule } from './register-market-schedule';
 import { MarketScheduleRegistered } from './market-schedule-registered';
+
+type CalendarEvent = MarketScheduleRegistered;
+
+class Calendar extends Aggregate {
+  apply(event: CalendarEvent): void {
+    // TODO implement
+  }
+
+  registerMarketSchedule(scheduleName: string, marketId: string, directionsToStall: string, days: { day: string, startTime?: string, endTime?: string }[]): void {
+    const event: MarketScheduleRegistered = {
+      type: 'MarketScheduleRegistered',
+      payload: {
+        scheduleName,
+        marketId,
+        directionsToStall,
+        days
+      }
+    };
+    this.raise(event);
+  }
+}
 
 export class RegisterMarketScheduleHandler {
   constructor(private readonly store: EventStore) {
   }
 
   async handle(registerMarketSchedule: RegisterMarketSchedule): Promise<void> {
-    const event: MarketScheduleRegistered = {
-      type: 'MarketScheduleRegistered',
-      payload: {
-        scheduleName: registerMarketSchedule.scheduleName,
-        marketId: registerMarketSchedule.marketId,
-        directionsToStall: registerMarketSchedule.directionsToStall,
-        days: registerMarketSchedule.days
-      }
-    };
-    await this.store.append('', [{ event }], 0);
+    const calendar = new Calendar();
+    calendar.registerMarketSchedule(
+      registerMarketSchedule.scheduleName,
+      registerMarketSchedule.marketId,
+      registerMarketSchedule.directionsToStall ?? '',
+      registerMarketSchedule.days
+    );
+    await this.store.append('', calendar.raisedEvents().map(event => ({ event })), 0);
   }
 }
