14b2c89dc6a42e02705a8a7aa48c8af5b346ecf0
w.alexander <will_alexander@me.com>
2026-06-07 14:13:17 +0200

    Disable linting on generated Astro file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*'],
+    ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*', '**/.astro'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
