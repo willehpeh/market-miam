ecff7a48dc7640df74f6efb747254f104a9f8deb
w.alexander <will_alexander@me.com>
2026-06-08 22:33:47 +0200

    Rebuild vendor-frontend app to remove SSR

--- apps/vendor-frontend/src/app/app.config.ts
+++ apps/vendor-frontend/src/app/app.config.ts
@@ -4,23 +4,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
-import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideClientHydration(withEventReplay()),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideAuth0({
-      domain: "willalexander.eu.auth0.com",
-      clientId: "8Aotn34QyliBSeJbDSK9VMuZOiwM7WuY",
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-    }),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
 };
--- apps/vendor-frontend/src/app/app.ts
+++ apps/vendor-frontend/src/app/app.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcome } from './nx-welcome';
 
 @Component({
+  imports: [NxWelcome, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  protected title = 'vendor-frontend';
+}
