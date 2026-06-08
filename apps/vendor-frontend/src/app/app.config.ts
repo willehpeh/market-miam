cebd00ba0f166298bf8fbe986ebb291ff73e3b31
w.alexander <will_alexander@me.com>
2026-06-08 22:22:27 +0200

    Add Auth0 to vendor frontend app config

--- apps/vendor-frontend/src/app/app.config.ts
+++ apps/vendor-frontend/src/app/app.config.ts
@@ -8,11 +8,19 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
+    provideAuth0({
+      domain: "willalexander.eu.auth0.com",
+      clientId: "8Aotn34QyliBSeJbDSK9VMuZOiwM7WuY",
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
 };
