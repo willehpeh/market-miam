07abad27c598255ef755ad44ddbec4745f2fd182
w.alexander <will_alexander@me.com>
2026-06-16 23:05:21 +0200

    Remove Angular boilerplate from admin app

--- apps/admin-frontend/src/app/app.ts
+++ apps/admin-frontend/src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'admin-frontend';
 }
